57f458e2d3380a0e0dd9b3f995514e1c47e72631
Mohamed Amr <MohxmedAmr@gmail.com>
2026-08-16 21:06:34 +0300

    Imporving DX with Vite HMR and npm-run-all

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base:"./",
+
+  base: command === "build" ? "./" : "/",
+
   build: {
     outDir: "dist-react",
-  }
-})
+  },
+
+  server: {
+    port: 5123,
+    strictPort: true,
+  },
+}));
